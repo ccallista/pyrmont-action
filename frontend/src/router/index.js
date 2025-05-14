60c2ba67c922ee19b91b91a85f93469ab50e2f69
callya <cleocwijaya@gmail.com>
2025-05-15 09:33:51 +1000

    [feature/fix-defects-contact-us-and-scroll] configure pages to reload at the top

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,9 @@ import { routes } from './routes'          // the array we just built
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  }
 })
 
 export default router
